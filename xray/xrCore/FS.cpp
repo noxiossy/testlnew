d8dce3699932466ce7736cfcf3f1ea49f94eb1e5
noxiossy <72839483+noxiossy@users.noreply.github.com>
2021-02-01 08:41:54 +0700

    Update FS.cpp

--- xray/xrCore/FS.cpp
+++ xray/xrCore/FS.cpp
@@ -339,7 +339,10 @@ IReader*	IReader::open_chunk(u32 ID)
 	} else return 0;
 };
 void	IReader::close()
-{	xr_delete((IReader*)this); }
+{
+	IReader* temp = this;
+	xr_delete(temp);
+}
 
 #include "FS_impl.h"
 
