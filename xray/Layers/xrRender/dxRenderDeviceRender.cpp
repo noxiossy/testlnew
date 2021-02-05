197102036beb363cf75046bc168535ef57987113
noxiossy <cari0us@outlook.com>
2021-02-05 09:27:16 +0700

    г

--- xray/Layers/xrRender/dxRenderDeviceRender.cpp
+++ xray/Layers/xrRender/dxRenderDeviceRender.cpp
@@ -62,7 +62,15 @@ void  dxRenderDeviceRender::Reset( HWND hWnd, u32 &dwWidth, u32 &dwHeight, float
 
 	Resources->reset_begin	();
 	Memory.mem_compact		();
+	
+    const bool noTexturesInRAM = RImplementation.o.no_ram_textures;
+    if (noTexturesInRAM)
+        ResourcesDeferredUnload();
+
 	HW.Reset				(hWnd);
+
+    if (noTexturesInRAM)
+        ResourcesDeferredUpload();
 
 #if defined(USE_DX10) || defined(USE_DX11)
 	dwWidth					= HW.m_ChainDesc.BufferDesc.Width;
@@ -242,6 +250,11 @@ void dxRenderDeviceRender::DeferredLoad(BOOL E)
 void dxRenderDeviceRender::ResourcesDeferredUpload()
 {
 	Resources->DeferredUpload();
+}
+
+void dxRenderDeviceRender::ResourcesDeferredUnload()
+{
+    Resources->DeferredUnload();
 }
 
 void dxRenderDeviceRender::ResourcesGetMemoryUsage(u32& m_base, u32& c_base, u32& m_lmaps, u32& c_lmaps)
