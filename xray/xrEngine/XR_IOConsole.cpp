52a806b40d3efda2155d119c2155ca5c9270d264
noxiossy <72839483+noxiossy@users.noreply.github.com>
2021-02-06 10:24:07 +0700

    Update XR_IOConsole.cpp

--- xray/xrEngine/XR_IOConsole.cpp
+++ xray/xrEngine/XR_IOConsole.cpp
@@ -15,8 +15,6 @@
 
 #include "../Include/xrRender/UIRender.h"
 
-#include "securom_api.h"
-
 static float const UI_BASE_WIDTH	= 1024.0f;
 static float const UI_BASE_HEIGHT	= 768.0f;
 static float const LDIST            = 0.05f;
@@ -179,7 +177,7 @@ void CConsole::OutFont( LPCSTR text, float& pos_y )
 		int ln	= 0;
 		PSTR one_line = (PSTR)_alloca( (CONSOLE_BUF_SIZE + 1) * sizeof(char) );
 		
-		while( text[sz] && (ln + sz < CONSOLE_BUF_SIZE-5) )// ïåðåíîñ ñòðîê
+		while( text[sz] && (ln + sz < CONSOLE_BUF_SIZE-5) )// ÄÄºÄ‘ÄºÃ­Ã®Å„ Å„ÅˆÄ‘Ã®Ä™
 		{
 			one_line[ln+sz]   = text[sz];
 			one_line[ln+sz+1] = 0;
@@ -606,8 +604,6 @@ void CConsole::ExecuteCommand( LPCSTR cmd_str, bool record_cmd )
 
 void CConsole::Show()
 {
-	SECUROM_MARKER_HIGH_SECURITY_ON(11)
-
 	if ( bVisible )
 	{
 		return;
@@ -625,8 +621,6 @@ void CConsole::Show()
 	m_editor->IR_Capture();
 	Device.seqRender.Add( this, 1 );
 	Device.seqFrame.Add( this );
-
-	SECUROM_MARKER_HIGH_SECURITY_OFF(11)
 }
 
 extern CInput* pInput;
